7f26fff8ebe2f8f755d8ca8d5dbc4203fa2df8f1
Angie Garcia <Angiegarciaoficial@gmail.com>
2020-08-23 01:35:40 -0500

    usando useEffect: limpiando eventos

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
   useEffect(function () {
     window.fetch('https://petgram-vercel-angie-k8crxss1m.vercel.app/categories')
       .then(res => res.json())
@@ -11,12 +12,31 @@ export const ListOfCategories = () => {
         setCategories(response)
       })
   }, [])
-  return (
-    <List>
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed &&
+      setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
 
     </List>
+  )
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
+
   )
 }
