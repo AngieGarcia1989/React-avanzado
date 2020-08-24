c23de8066084980116dd5c20a577db02a718a52d
Angie Garcia <Angiegarciaoficial@gmail.com>
2020-08-24 16:25:53 -0500

    Uso de polyfill de Intersection Observer e imports dinámicos

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -3,26 +3,27 @@ import { Article, ImgWrapper, Img, Button } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_cats.jpg'
-// se coloca la foto de archivo j.son
+
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const element = useRef(null)
   const [show, setShow] = useState(false)
   useEffect(function () {
-    // console.log(element.current)
-    const observer = new window.IntersectionObserver(function (entries) {
-      // console.log(entries) para saber si esta en pantalla del usuario
-      const { isIntersecting } = entries[0]
-      // console.log({ isIntersecting }) representa con true o false cuando esta la foto en el view y lo va actualizando a medida que cambia con el scroll
-      console.log(isIntersecting)
-      if (isIntersecting) {
-        // console.log('si')
-        setShow(true)
-        observer.disconnect()
-        // de esta manera cuando sea true observer deja de funcionar
-      }
-    })
-    observer.observe(element.current)
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer'))
+      .then(() => {
+        const observer = new window.IntersectionObserver(function (entries) {
+          const { isIntersecting } = entries[0]
+          if (isIntersecting) {
+            setShow(true)
+            observer.disconnect()
+          }
+        })
+        observer.observe(element.current)
+      })
   }, [element])
+
   return (
     <Article ref={element}>
       {
