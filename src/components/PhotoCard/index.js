426e402b97be1cd33a15dd5c9320fe79edc5d466
Angie Garcia <Angiegarciaoficial@gmail.com>
2020-08-24 14:49:09 -0500

    Usando Instersection Observer

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,21 +1,44 @@
-import React from 'react'
-import { ImgWrapper, Img, Button } from './styles'
+import React, { useEffect, useRef, useState } from 'react'
+import { Article, ImgWrapper, Img, Button } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 
-const DEFAULT_IMAGE = 'https://www.mundoperros.es/wp-content/uploads/2017/09/Pomerania.jpg'
-
-export const PhotoCard = ({ id, likes = 0, src }) => {
+const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_cats.jpg'
+// se coloca la foto de archivo j.son
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
+  useEffect(function () {
+    // console.log(element.current)
+    const observer = new window.IntersectionObserver(function (entries) {
+      // console.log(entries) para saber si esta en pantalla del usuario
+      const { isIntersecting } = entries[0]
+      // console.log({ isIntersecting }) representa con true o false cuando esta la foto en el view y lo va actualizando a medida que cambia con el scroll
+      console.log(isIntersecting)
+      if (isIntersecting) {
+        // console.log('si')
+        setShow(true)
+        observer.disconnect()
+        // de esta manera cuando sea true observer deja de funcionar
+      }
+    })
+    observer.observe(element.current)
+  }, [element])
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={DEFAULT_IMAGE} />
-        </ImgWrapper>
-      </a>
+    <Article ref={element}>
+      {
+        show &&
+          <>
+            <a href={`/detail/${id}`}>
+              <ImgWrapper>
+                <Img src={src} />
+              </ImgWrapper>
+            </a>
+            <Button>
+              <MdFavoriteBorder size='32px' /> {likes} likes!
+            </Button>
+          </>
+      }
 
-      <Button>
-        <MdFavoriteBorder size='32px' /> {likes} likes!
-      </Button>
-    </article>
+    </Article>
   )
 }
