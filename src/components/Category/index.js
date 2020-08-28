0d7d8533f94274d953167cb1522adde0e8724afd
Angie Garcia <Angiegarciaoficial@gmail.com>
2020-08-28 17:57:42 -0500

    Usando Link para evitar recargar la página

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Anchor, Image } from './styles'
+import { Link, Image } from './styles'
 
 const DEFAULT_IMAGE = 'https://www.publicdomainpictures.net/pictures/180000/velka/dog-foodprint.jpg'
 
-export const Category = ({ cover = DEFAULT_IMAGE, path, emoji = 'Cargando...' }) => (
-  <Anchor href={path}>
+export const Category = ({ cover = DEFAULT_IMAGE, path = '#', emoji = 'Cargando...' }) => (
+  <Link to={path}>
     <Image src={cover} />
     {emoji}
-  </Anchor>
+  </Link>
 )
