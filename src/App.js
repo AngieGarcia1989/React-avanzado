43d49ff32a2fcdf9db457841976f6cbd98b0ab96
Angie Garcia <Angiegarciaoficial@gmail.com>
2020-08-28 18:18:55 -0500

    Creando la página Detail

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React, { Fragment } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Home } from './pages/Home'
+import { Detail } from './pages/Deatail'
 import { Router } from '@reach/router'
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
-
   return (
     <div>
       <GlobalStyle />
       <Logo />
-      {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : <Router>
-            <Home path='/' />
-            <Home path='/pet/:id' />
-          </Router>
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:id' />
+        <Detail path='/detail/:detailId' />
+      </Router>
 
-      }
     </div>
   )
 }
