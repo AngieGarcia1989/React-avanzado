e3f77eb796f67a8ee34f9c40d032c018725b1edf
Angie Garcia <Angiegarciaoficial@gmail.com>
2020-08-28 19:27:47 -0500

    Agregando un NavBar a nuestra app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
+import { NavBar } from './components/NavBar'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Deatail'
 import { Router } from '@reach/router'
@@ -14,6 +15,7 @@ export const App = () => {
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <NavBar />
 
     </div>
   )
