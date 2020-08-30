c8966fc9aa5d89ef6c35d0d4eef2ecd7dff718cb
Angie Garcia <Angiegarciaoficial@gmail.com>
2020-08-30 13:20:17 -0500

    Mutaciones para registro

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -6,10 +6,18 @@ export const UserForm = ({ onSubmit, title }) => {
   const email = useInputValue('')
   const password = useInputValue('')
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    onSubmit({
+      email: email.value,
+      password: password.value
+    })
+  }
+
   return (
     <>
       <Title>{title}</Title>
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={handleSubmit}>
         <Input placeholder='Email' value={email.value} {...email} />
         <Input placeholder='Password' type='password' value={password.value} {...password} />
         <Button>{title}</Button>
