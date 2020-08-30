9af2ea404af4fed9925e3f000feeacab339d60b6
Angie Garcia <Angiegarciaoficial@gmail.com>
2020-08-30 15:40:01 -0500

    Mutaciones para iniciar sesión

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Context from '../Context'
 import { UserForm } from '../components/UserForm'
 import { RegisterMutation } from '../container/RegisterMutation'
+import { LoginMutation } from '../container/LoginMutation'
 
 export const NotRegisteredUser = () => (
   <Context.Consumer>
@@ -24,10 +25,22 @@ export const NotRegisteredUser = () => (
             }
 
           </RegisterMutation>
+          <LoginMutation>
+            {
+              (login, { data, loading, error }) => {
+                const onSubmit = ({ email, password }) => {
+                  const input = { email, password }
+                  const variables = { input }
+                  login({ variables }).then(activateAuth)
+                }
 
-          <UserForm title='Iniciar Sesión' onSubmit={activateAuth} />
+                const errorMsg = error && 'La constraseña no es correcta o el Usuario No existe.'
+                return <UserForm disabled={loading} error={errorMsg} title='Iniciar Sesión' onSubmit={onSubmit} />
+              }
+            }
 
-        </>
+          </LoginMutation>
+               </>
       }
     }
 
